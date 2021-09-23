b43a92373cd6c7ea5cfce19c0be86853f1642f36
Jimmy Siljelind <jimmy.alexander.siljelind@gmail.com>
2021-09-23 11:18:43 +0200

    Game Over

--- DungeonCrawler/GameEngine.cs
+++ DungeonCrawler/GameEngine.cs
@@ -140,8 +140,7 @@ namespace DungeonCrawler
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
             Console.WriteLine();
-            Console.WriteLine("GAME OVER");
-            Console.WriteLine("YOU LOSE BIG TIME!");
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "GAME OVER\"YOU LOSE BIG TIME!"));
         }
 
         public void DrawVictoryScreen()
@@ -150,8 +149,7 @@ namespace DungeonCrawler
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
             Console.WriteLine();
-            Console.WriteLine("A WINNER IS YOU!");
-            Console.WriteLine("YOU FOUND ENOUGH COINS TO RETIRE IN LUXURY!");
+            Console.WriteLine(String.Format("{0," + Console.WindowWidth / 2 + "}", "A WINNER IS YOU!\"YOU FOUND ENOUGH COINS TO RETIRE IN LUXURY!"));
         }
     }
 }
